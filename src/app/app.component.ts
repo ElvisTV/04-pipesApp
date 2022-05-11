08410f3b553fe5e6437da63ae68433f001cfe01c
Elvis Tezen <elvistezen@outlook.es>
2022-05-11 12:56:25 -0500

    hasta class 139

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+import {MenuItem} from 'primeng/api';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  nombre: string = "Elvi Arturo Tezén Villanueva";
+  valor: number = 1000;
+  obj = {
+    nombre: 'Fernando'
+  }
+
+  mostrarNombre() {
+    console.log(this.nombre);
+  }
+
   title = 'pipesApp';
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+//Modulo personalizado
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
